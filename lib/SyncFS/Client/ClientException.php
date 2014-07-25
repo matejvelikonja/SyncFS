d54735abfa69a28147c25d75ae58a7c26ea22753
matejvelikonja <matej@velikonja.si>
2014-07-26 00:25:09 +0200

    Replaces rsync external lib with custom one.

---
+++ lib/SyncFS/Client/ClientException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace SyncFS\Client;
+
+/**
+ * Class ClientException
+ *
+ * @package SyncFS\ClientException
+ * @author  Matej Velikonja <matej@velikonja.si>
+ */
+class ClientException extends \Exception
+{
+
+} 
--- lib/SyncFS/Client/RsyncClient.php
+++ lib/SyncFS/Client/RsyncClient.php
@@ -2,7 +2,7 @@
 
 namespace SyncFS\Client;
 
-use AFM\Rsync\Rsync;
+use Symfony\Component\Process\Process;
 
 /**
  * Class RsyncClient
@@ -10,7 +10,95 @@ use AFM\Rsync\Rsync;
  * @package SyncFS\Client
  * @author  Matej Velikonja <matej@velikonja.si>
  */
-class RsyncClient extends Rsync implements ClientInterface
+class RsyncClient implements ClientInterface
 {
+    /**
+     * @var array
+     */
+    private $options;
 
+    /**
+     * @var string
+     */
+    private $executable;
+
+    /**
+     * @param array $options Use options from rSync manual page
+     *
+     * @link http://linux.die.net/man/1/rsync
+     */
+    public function __construct(array $options = array())
+    {
+        $defaultOptions = array(
+            'rsync' => array(
+                'archive'        => true,
+                'verbose'        => true,
+                'compress'       => true,
+                'human-readable' => true,
+                'delete'         => false,
+                'progress'       => true,
+            ),
+            'timeout' => 60
+        );
+
+        $this->options    = array_replace_recursive($defaultOptions, $options);
+        $this->executable = '/usr/bin/rsync';
+    }
+
+    /**
+     * @param string $src
+     * @param string $dst
+     *
+     * @throws ClientException
+     *
+     * @return int|null
+     */
+    public function sync($src, $dst)
+    {
+        if (! strlen($dst)) {
+            throw new ClientException('Destination parameter missing.');
+        }
+
+        if (! strlen($src)) {
+            throw new ClientException('Source parameter missing.');
+        }
+
+        $options = $this->getOptionsString();
+        $command = implode(' ', array($this->executable, $options, $src, $dst));
+        $process = new Process($command);
+        $process->setTimeout($this->options['timeout']);
+
+        $process->run(function ($type, $buffer) {
+            echo $buffer . PHP_EOL;
+        });
+
+        if (! $process->isSuccessful()) {
+            throw new ClientException($process->getErrorOutput());
+        }
+
+        return $process->getExitCode();
+    }
+
+    /**
+     * @return string
+     */
+    private function getOptionsString()
+    {
+        $options   = array_filter($this->options['rsync']); // remove all options with false value
+        $arguments = '';
+
+        foreach($options as $name => $value) {
+            $prefix = ' -';
+            if (strlen($name) > 1) {
+                $prefix = ' --';
+            }
+
+            $arguments .= $prefix . $name;
+            if ($value && !is_bool($value)) {
+                $arguments .= '=' . $value;
+            }
+        }
+
+        return trim($arguments);
+    }
 }
